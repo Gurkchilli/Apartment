5c8a9493e9e0270dc0732bf93aa26816f2373efb
Erik Hildinge <erik.hildinge@live.se>
2021-04-16 21:27:41 +0200

    hide/show the apartments instead of teleporting the camera

--- Assets/CameraMover.cs
+++ Assets/CameraMover.cs
@@ -4,40 +4,49 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
-    public Transform tf;
-    private Vector3 standardCam = new Vector3(3.38f, 0.978f, 2.861f);
-    private Vector3 standardRot = new Vector3(0,60,0);
-
-    private Vector3 a1 = new Vector3(0,0,0);
-    private Vector3 a2 = new Vector3(20, 0, 0);
-    private Vector3 a3 = new Vector3(40, 0, 0);
-    private Vector3 a4 = new Vector3(60, 0, 0);
-    private Vector3 a5 = new Vector3(80, 0, 0);
+    public GameObject[] goArr;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 currentPos = tf.localEulerAngles;
         if (Input.GetKey(KeyCode.Alpha1))
         {
-            tf.localPosition = a1 + standardCam;
+            HideGos();
+            SetActiveGo(0);
         }
         if (Input.GetKey(KeyCode.Alpha2))
         {
-            tf.localPosition = a2 + standardCam;
+            HideGos();
+            SetActiveGo(1);
         }
         if (Input.GetKey(KeyCode.Alpha3))
         {
-            tf.localPosition = a3 + standardCam;
+            HideGos();
+            SetActiveGo(2);
         }
         if (Input.GetKey(KeyCode.Alpha4))
         {
-            tf.localPosition = a4 + standardCam;
+            HideGos();
+            SetActiveGo(3);
         }
         if (Input.GetKey(KeyCode.Alpha5))
         {
-            tf.localPosition = a5 + standardCam;
+            HideGos();
+            SetActiveGo(4);
         }
 
+    }
+
+    private void HideGos()
+    {
+        foreach(GameObject go in goArr)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    private void SetActiveGo(int index)
+    {
+        goArr[index].SetActive(true);
     }
 }
